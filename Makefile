2f7cdac0f4a780df8aad12e9bf941c2b08e70f34
“darkblackv32” <angello.soldi@utec.edu.pe>
2023-07-26 01:33:50 -0500

    001

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
+sll:
+	g++ -std=c++20 linked_list.cpp && ./a.out && rm ./a.out
 rev:
 	g++ -std=c++20 pointers.cpp && ./a.out && rm ./a.out
--- linked_list.cpp
+++ linked_list.cpp
@@ -1,25 +1,82 @@
 #include <bits/stdc++.h>
+#include <iomanip>
 using namespace std;
 
 
-template<typename T>
+// template<typename T>
 
-struct Node{
-    T value;
-    Node *next;
+struct node{
+    int value;
+    node *nextId;
 };
 
 
 
-template<typename T>
-class List{
+// template<typename T>
+class SLL{
 private:
-    Node *head, *tail;
+    node *head;
+    node *tail;
 public:
-    list(){
-        *tail = nullptr;
-        *head = nullptr;
+    SLL(){
+        tail = nullptr;
+        head = nullptr;
+    }
+
+    bool is_empty(){
+        return (head == nullptr);
+    }
+
+    void pb(int val){
+
+        node* newNode = new node;
+        newNode->value = val; 
+        newNode->nextId = nullptr; //! cos it's the last element
+
+        if(!is_empty()){
+            node *curr = head;
+            while(curr->nextId != nullptr){
+                curr = curr->nextId;
+            }
+            curr->nextId = newNode;
+        }
+
+
+        else head = newNode;
+
+
+    }
+
+    void print(){
+        node *curr = head;
+        while(curr != nullptr)
+        {
+            cout << curr->value;
+            curr = curr->nextId;
+        }
     }
 
 };
+
+int main(){
+
+    SLL l1;
+
+    cout << boolalpha << l1.is_empty();
+
+    l1.pb(8);
+    l1.pb(10);
+
+    cout << boolalpha << l1.is_empty()<<endl;
+
+    l1.print();
+    
+
+
+
+
+
+
+    return 0;
+}
 
