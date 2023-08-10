a52c940a49d655ff6cfd23492a1c683838002333
“darkblackv32” <angello.soldi@utec.edu.pe>
2023-08-09 20:53:56 -0500

    h1

--- homework/circular_array/circulararray.h
+++ homework/circular_array/circulararray.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
+
 using namespace std;
 
 template <class T>
@@ -12,39 +13,126 @@ protected:
     int capacity;       // for table doubling
     int back, front;    // indirection markers
 public:
-    CircularArray();
+    CircularArray(){
+        array = new T[size_];
+        capacity = 0;
+        back, front = 0;
+    };
     CircularArray(int _capacity){
+        array = new T[size_];
         capacity = _capacity;
+        back, front = 0;
     };
     
-    virtual ~CircularArray(){};
+    virtual ~CircularArray(){
+        delete[] array;
+    };
+
 
     void push_front(T data){
-        
+        if(!is_full()){
+            
+            front = prev(front);
+            array[front] = data;
+            size_++;
+        }
+        else {
+            resize(capacity*2);
+            push_front(data);
+        }
+
     };
-    void push_back(T data);
+
+    void push_back(T data){
+        if(!is_full()){
+            array[back] = data;
+            back = next(back);
+            size_++;
+        } else{
+            resize(capacity*2);
+            push_back(data);}
+    };
+
     void insert(T data, int pos);
-    T pop_front();
-    T pop_back();
-    
-    bool is_full();
-    bool is_empty(){
-        return (size_ == 0);
+
+    T pop_front(){
+        if(!is_empty()){
+            T aux = array[front];
+            front = next(front);
+            size_--;
+            return aux;
+        }
+        return T();
     };
-    int size();
+
+    T pop_back(){
+        if(!is_empty()){
+            back = prev(back);
+            T aux = array[back];
+            size_--;
+            return aux;
+        }
+       return T();
+    };
     
-    void resize(int new_capacity);
+    bool is_full(){
+        return next(back) == front;
+    };
+    bool is_empty(){
+        return size_ == 0;
+    };
+    int size(){
+        return size_;
+    };
+    
+    void resize(int new_capacity){
+        if(new_capacity > capacity){
+            T* new_array = new T[new_capacity];
+            int aux = front;
+            for (int i = 0; i < size_; i++){
+                new_array[i] = array[aux];
+                aux = next(aux);
+            }
+            delete [] array;
+            array = new_array;
+            front = 0;
+            back = size_;
+            capacity = new_capacity;
+        }
+    };
+
+
     void clear();
     T &operator[](int index);
     void sort();
     bool is_sorted();
     void reverse();
-    string to_string(string sep=" ");       // overload << operator
+
+    string to_string(string sep = " ") {
+        string result;
+        int idx = front;
+        for (int i = 0; i < size_; i++) {
+            if (i > 0) {
+                result += sep;
+            }
+            result += std::to_string(array[idx]);
+            idx = next(idx);
+        }
+        return result;
+    }
+       
     
     T front_() { return array[this->front]; }
     T back_() { return array[this->back]; }
+
 private:
     // ? use modulus %
-    int next(int);      // (front+1) % capacity
-    int prev(int);      // 
+    int next(int i){
+        return (i + 1) % capacity; // (front+1) % capacity
+    };      
+
+    int prev(int i){
+        return (i - 1 + capacity) % capacity;
+    };      // 
 };
+
